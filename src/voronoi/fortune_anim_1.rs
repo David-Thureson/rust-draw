b7e2e22c56c81f628751c6ccc4428e09ea14c02e
David Thureson <David.G.Thureson@gmail.com>
2021-08-04 23:00:17 -0700

    Added rasterization for parabolas.

--- src/voronoi/fortune_anim_1.rs
+++ src/voronoi/fortune_anim_1.rs
@@ -7,7 +7,6 @@ use crate::grid::Grid;
 use crate::{Color1, Frame};
 use crate::renderer_3::Renderer;
 use crate::*;
-use num::traits::Pow;
 
 const COLOR_BACKGROUND: usize      = 0;
 const COLOR_DIRECTRIX: usize       = 1;
@@ -49,12 +48,7 @@ struct ParabolaList {
 struct Parabola {
     pub focus_x: f64,
     pub focus_y: f64,
-    pub directrix_y: f64,
-    pub p: f64,
-    pub h: f64,
-    pub k: f64,
-    pub y_max: f64,
-    pub state: ParabolaState,
+    pub state: ParabolaState
 }
 
 #[derive(Clone)]
@@ -105,15 +99,8 @@ impl ParabolaList {
         self.parabolas.insert(focus_x.into(), Parabola::new(focus_x, focus_y));
     }
 
-    fn update_parabolas(&mut self, directrix_y: f64) {
-        for parabola in self.parabolas.values_mut() {
-            parabola.update(directrix_y);
-        }
-    }
-
     pub fn add_to_shapes(&mut self, shapes: &mut Vec<Shape>, width: usize, height: usize, directrix_y: f64) {
         // Update the parabolas' states.
-        self.update_parabolas(directrix_y);
         let ref_parabolas = self.parabolas.clone();
         for parabola in self.parabolas.values_mut() {
             if parabola.focus_y > directrix_y {
@@ -123,7 +110,7 @@ impl ParabolaList {
                 parabola.state = ParabolaState::Active;
                 // See if the parabola is contained in any other parabola.
                 for ref_parabola in ref_parabolas.values() {
-                    if ref_parabola.contains(parabola,None) {
+                    if ref_parabola.contains(parabola, directrix_y, None) {
                         parabola.state = ParabolaState::Contained;
                         break;
                     }
@@ -131,18 +118,17 @@ impl ParabolaList {
             }
         }
         for parabola in self.parabolas.values() {
-            parabola.add_to_shapes(shapes, width, height);
+            parabola.add_to_shapes(shapes, width, height, directrix_y);
         }
     }
 
-    pub fn debug_contains(&mut self, directrix_y: f64) {
-        self.update_parabolas(directrix_y);
+    pub fn debug_contains(&self, directrix_y: f64) {
         let ref_parabolas = self.parabolas.clone();
         for (index, parabola) in self.parabolas.values().enumerate() {
             println!("\n[{}] ({}, {})", index, parabola.focus_x.round(), parabola.focus_y.round());
             for (ref_index, ref_parabola) in ref_parabolas.values().enumerate() {
-                if ref_parabola.contains(parabola, None) {
-                    ref_parabola.contains(parabola, Some(ref_index));
+                if ref_parabola.contains(parabola, directrix_y, None) {
+                    ref_parabola.contains(parabola, directrix_y, Some(ref_index));
                 }
             }
         }
@@ -155,23 +141,8 @@ impl Parabola {
         Self {
             focus_x: focus_x,
             focus_y: focus_y,
-            directrix_y: 0.0,
-            p: 0.0,
-            h: 0.0,
-            k: 0.0,
-            y_max: 0.0,
             state: ParabolaState::Pending,
         }
-    }
-
-    #[inline]
-    pub fn update(&mut self, directrix_y: f64) {
-        self.directrix_y = directrix_y;
-        self.p = self.p(directrix_y);
-        let (h, k) = self.h_k(directrix_y);
-        self.h = h;
-        self.k = k;
-        self.y_max = self.focus_y - self.p;
     }
 
     #[inline]
@@ -189,35 +160,24 @@ impl Parabola {
         (self.focus_x, self.y_max(directrix_y))
     }
 
-    #[inline]
-    pub fn y(&self, x: f64) -> f64 {
-        let num = (x * x) + (-2.0 * x * self.h) + (self.h * self.h) + (4.0 * self.p * self.k);
-        let denom = 4.0 * self.p;
-        let y = num / denom;
-        y
-    }
-
-    #[inline]
-    pub fn error(&self, x: usize, y: usize) -> f64 {
-        let (x, y) = (x as f64, y as f64);
-        (x - self.h).pow(2) + (-4.0 * self.p * (y - self.k))
-    }
-
-    pub fn x_coords(&self, y: f64, debug: bool) -> Vec<f64> {
-        if debug { println!("\t\tx_coords(): directrix_y = {}; y = {}; y_max = {}", self.directrix_y.round(), y.round(), self.y_max.round()); }
+    pub fn x_coords(&self, directrix_y: f64, y: f64, debug: bool) -> Vec<f64> {
+        let y_max = self.y_max(directrix_y);
+        if debug { println!("\t\tx_coords(): directrix_y = {}; y = {}; y_max = {}", directrix_y.round(), y.round(), y_max.round()); }
         let mut v = vec![];
-        if y == self.y_max {
+        if y == y_max {
             v.push(self.focus_x);
         } else {
-            if y < self.y_max {
+            if y < y_max {
+                let (h, k) = self.h_k(directrix_y);
+                let p = self.p(directrix_y);
                 // (x-h)^2 = 4p(y-k)
                 // x^2 - 2xh + h^2 - 4p(y-k) = 0
                 // So for the quadratic formula:
                 let a: f64 = 1.0;
-                let b: f64 = -2.0 * self.h;
+                let b: f64 = -2.0 * h;
                 // let c: f64 = 4.0 * p * (y - k);
-                let c: f64 = (self.h * self.h) + (-4.0 * self.p * (y - self.k));
-                if debug { println!("\t\t\tp = {}, h = {}, k = {}, a = {}, b = {}, c = {}", self.p.round(), self.h.round(), self.k.round(), a.round(), b.round(), c.round()); }
+                let c: f64 = (h * h) + (-4.0 * p * (y - k));
+                if debug { println!("\t\t\tp = {}, h = {}, k = {}, a = {}, b = {}, c = {}", p.round(), h.round(), k.round(), a.round(), b.round(), c.round()); }
                 let discriminant = discriminant(a, b, c);
                 assert!(discriminant > 0.0);
                 let roots = quadratic_roots(a, b, c);
@@ -233,23 +193,24 @@ impl Parabola {
         v
     }
 
-    pub fn contains(&self, other: &Self, debug_self_index: Option<usize>) -> bool {
+    pub fn contains(&self, other: &Self, directrix_y: f64, debug_self_index: Option<usize>) -> bool {
         let debug = debug_self_index.is_some();
         if debug { println!("\n\t[{}] ({}, {}) contains ({}, {}):", debug_self_index.unwrap(), self.focus_x.round(), self.focus_y.round(), other.focus_x.round(), other.focus_y.round()); }
-        if self.focus_y > self.directrix_y || other.focus_y > self.directrix_y {
+        if self.focus_y > directrix_y || other.focus_y > directrix_y {
             // One or both parabolas is beyond the directrix and thus doesn't count yet.
             if debug { println!("\t\tOne or more below the directrix."); }
             return false;
         }
-        if debug { println!("\t\tself.y_max = {}; other.y_max = {}", self.y_max.round(), other.y_max.round()); }
-        if self.y_max <= other.y_max {
+        let (self_y_max, other_y_max) = (self.y_max(directrix_y), other.y_max(directrix_y));
+        if debug { println!("\t\tself_y_max = {}; other_y_max = {}", self_y_max.round(), other_y_max.round()); }
+        if self_y_max <= other_y_max {
             // The current parabola must be below (higher y) the other parabola to contain it.
             if debug { println!("\t\tCurrent parabola is not below other parabola."); }
             return false;
         }
         // Find the left and right points on the current parabola at the same y coordinate as the
         // maximum point of the other parabola.
-        let mut x_coords = self.x_coords(other.y_max, debug);
+        let mut x_coords = self.x_coords(directrix_y, other_y_max, debug);
         assert_eq!(2, x_coords.len());
         if debug { println!("\t\tx_coords = [{}, {}]", x_coords[0].round(), x_coords[1].round()); }
         let contains = other.focus_x > x_coords[0] && other.focus_x < x_coords[1];
@@ -257,7 +218,7 @@ impl Parabola {
         contains
     }
 
-    pub fn add_to_shapes(&self, shapes: &mut Vec<Shape>, width: usize, height: usize) {
+    pub fn add_to_shapes(&self, shapes: &mut Vec<Shape>, width: usize, height: usize, directrix_y: f64) {
         // Add the focus circle.
         let color_index = match self.state {
             ParabolaState::Active => COLOR_FOCUS_ACTIVE,
@@ -268,71 +229,19 @@ impl Parabola {
 
         match self.state {
             ParabolaState::Active => {
-                // Draw the right half in the old way.
-                for x in self.focus_x.round() as usize..width {
+                for x in 0..width {
                     let x = x as f64;
+                    let (h, k) = self.h_k(directrix_y);
+                    let p = self.p(directrix_y);
                     // (x-h)^2 = 4p(y-k)
                     // y = (x^2 - 2xh + h^2 + 4pk) / 4p
-                    let num = (x * x) + (-2.0 * x * self.h) + (self.h * self.h) + (4.0 * self.p * self.k);
-                    let denom = 4.0 * self.p;
+                    let num = (x * x) + (-2.0 * x * h) + (h * h) + (4.0 * p * k);
+                    let denom = 4.0 * p;
                     let y = (num / denom).round();
                     if y >= 0.0 && y < height as f64 {
                         shapes.push(Shape::rectangle_fast(x, y, 1.0, 1.0, COLOR_PARABOLA));
                     }
                 }
-                // Draw the left half using http://members.chello.at/easyfilter/bresenham.pdf, page 10.
-                // Note that the y axis is reversed so this is really an upward slope.
-                let (mut x, mut y) = {
-                    // Try x = 0 and see if y is on the screen.
-                    let x = 0.0;
-                    let y = self.y(x);
-                    if y > 0.0 {
-                        (x as usize, y.round() as usize)
-                    } else {
-                        // Instead start with y = 0 and find x.
-                        let y = 0.0;
-                        let x = self.x_coords(y, false)[0];
-                        (x.round() as usize, y as usize)
-                    }
-                };
-                let x_stop = self.focus_x.round() as usize;
-                while x < x_stop {
-                    //rintln!("x = {}, y = {}", x, y);
-                    shapes.push(Shape::rectangle_fast(x as f64, y as f64, 1.0, 1.0, COLOR_PARABOLA));
-                    let err_xy = self.error(x + 1, y + 1).abs();
-                    let err_x = self.error(x, y + 1).abs();
-                    let err_y = self.error(x + 1, y).abs();
-                    //rintln!("\terr_xy = {}, err_x = {}, err_y = {}, err_x + err_xy = {}, err_y + err_xy = {}", err_xy, err_x, err_y, err_x + err_xy, err_y + err_xy);
-                    /*
-                    let mut inc = false;
-                    if err_x + err_xy > 0.0 {
-                        x += 1;
-                        inc = true;
-                    }
-                    if err_y + err_xy > 0.0 {
-                        y += 1;
-                        inc = true;
-                    }
-                    if !inc {
-                        // panic!();
-                        break;
-                    }*/
-                    if err_xy < err_x {
-                        if err_xy < err_y {
-                            x += 1;
-                            y += 1;
-                        } else {
-                            x += 1;
-                        }
-                    } else {
-                        if err_x < err_y {
-                            y += 1;
-                        } else {
-                            x += 1;
-                        }
-                    }
-                }
-
                 // For debugging, draw a horizontal line through the focus and another several
                 // pixels higher.
                 /*
@@ -387,7 +296,6 @@ fn try_draw_parabola() {
 
 fn try_draw_parabolas() {
     let mut rng = thread_rng();
-    // let (width, height, parabola_count, anim_seconds, frame_count) = (800, 800, 10, 1, 1);
     // let (width, height, parabola_count, anim_seconds, frame_count) = (1_000, 1_000, 10, 20, 20);
     let (width, height, parabola_count, anim_seconds, frame_count) = (1_600, 800, 100, 60, 240);
     let (display_width, display_height) = (width as f64, height as f64);
@@ -399,6 +307,7 @@ fn try_draw_parabolas() {
     }
 
     let mut frames = vec![];
+
 
     // let directrix_y = height as f64 * 0.75;
 
